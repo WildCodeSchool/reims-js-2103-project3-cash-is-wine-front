64516bc1231160e88de58ae78cb7acf9056e40bb
AllanCaplain <ac.cppro51@gmail.com>
2021-06-02 16:19:26 +0200

    style of homepage, in progress

---
+++ src/components/Header.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import logoCash from './assets/logoCash.png';
+import './Header.css';
+
+const Header = () => (
+  <header className="header">
+    <img className="cashiswineImg" src={logoCash} alt="logo" />
+    <div className="thefourth">
+      <nav className="navlist">
+        <NavLink className="link" activeClassName="active" exact to="/">
+          Accueil
+        </NavLink>
+
+        <NavLink className="link" activeClassName="active" exact to="/vinothèque">
+          Vinothèque
+        </NavLink>
+
+        <NavLink className="link" activeClassName="active" exact to="/compte">
+          Compte
+        </NavLink>
+      </nav>
+    </div>
+    <div className="background">
+      <h1>Trouvez des vins prêts à boire accessibles</h1>
+    </div>
+  </header>
+);
+
+export default Header;
