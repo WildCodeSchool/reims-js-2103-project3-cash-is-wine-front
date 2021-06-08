dd45de3cb58c2941e1dd04fe9e6d743c3a8eea0c
MarjorieThomassin <marjorie.thomassin5@gmail.com>
2021-06-08 11:04:44 +0200

    feat: created WineList Context

--- src/App.jsx
+++ src/App.jsx
@@ -5,16 +5,19 @@ import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import Navbar from './components/Navbar';
 import { LoginDataProvider } from './contexts/LoginDataContext';
+import { WineListProvider } from './contexts/WineListContext';
 import SearchBar from './components/Searchbar';
 
 function App() {
   return (
     <LoginDataProvider>
-      <BrowserRouter>
-        <Navbar />
-        <Header />
-        <SearchBar />
-      </BrowserRouter>
+      <WineListProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Header />
+          <SearchBar />
+        </BrowserRouter>
+      </WineListProvider>
     </LoginDataProvider>
   );
 }
---
+++ src/contexts/WineListContext.jsx
@@ -0,0 +1,28 @@
+import React, { createContext, useContext, useState } from 'react';
+import PropTypes from 'prop-types';
+
+const WineListContext = createContext();
+
+// encapsulate the global state inside a provider
+
+function WineListProvider({ children }) {
+  const [wineList, setWineList] = useState(); // start from undefined: no logged user
+
+  return (
+    <WineListContext.Provider value={{ wineList, setWineList }}>
+      {children}
+    </WineListContext.Provider>
+  );
+}
+
+WineListProvider.propTypes = {
+  children: PropTypes.element.isRequired,
+};
+
+// make a shortcut for useContext
+
+const useWineList = () => useContext(WineListContext);
+
+// export helpers
+
+export { WineListProvider, useWineList };
