06da9f77965fd3a134a7b843b752dc41b88ebed4
Timothee <augustin.timothee@gmail.com>
2021-06-09 15:02:08 +0200

    feat: searchbar functionnal

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,30 +1,28 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
 import './Searchbar.css';
+import WineLister from './WineLister';
 
-function SearchBar({ getQuery }) {
+function SearchBar() {
   const [text, setText] = useState('');
 
-  const onChange = (q) => {
-    setText(q);
-    getQuery(q);
+  const onChange = (event) => {
+    setText(event.target.value);
   };
   return (
-    <div className="searchBar">
-      <input
-        type="search"
-        className="form-control"
-        placeholder="Rechercher votre bouteille"
-        results="0"
-        value={text}
-        onChange={() => onChange()}
-      />
-    </div>
+    <>
+      <div className="searchBar">
+        <input
+          type="search"
+          className="form-control"
+          placeholder="Rechercher votre bouteille"
+          results="0"
+          value={text}
+          onChange={onChange}
+        />
+      </div>
+      <WineLister text={text} />
+    </>
   );
 }
-
-SearchBar.propTypes = {
-  getQuery: PropTypes.func.isRequired,
-};
 
 export default SearchBar;
--- src/components/WineLister.jsx
+++ src/components/WineLister.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { useWineList } from '../contexts/WineListContext';
 import WineBottle from './WineBottle';
 
-function WineLister() {
+function WineLister({ text }) {
   const { wineList } = useWineList();
+  console.log(wineList);
+  console.log(typeof wineList);
+  console.log(text);
+  console.log(typeof text);
   return (
     <>
-      { wineList && wineList.map((wine) => (
-        <WineBottle key={wine.id} wine={wine} />
-      ))}
+      { wineList && wineList.filter((wine) => (wine.estate.toUpperCase()
+        .includes(text.toUpperCase())) || (wine.appellation.toUpperCase()
+        .includes(text.toUpperCase())) || (wine.color.toUpperCase()
+        .includes(text.toUpperCase())) || (wine.year.toString(10)
+        .includes(text)))
+        .map((wine) => (
+          <WineBottle key={wine.id} wine={wine} />
+        ))}
     </>
   );
 }
+
+WineLister.propTypes = {
+  text: PropTypes.string.isRequired,
+};
 
 export default WineLister;
