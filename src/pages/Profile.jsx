ab48ffc4ef5e91b915ea2e6fa2a94a6b0afdf7e0
Timothee <augustin.timothee@gmail.com>
2021-06-22 14:57:45 +0200

    feat: add a bottle from the form

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Redirect } from 'react-router-dom';
+import axios from 'axios';
 import ShowWinary from '../components/ShowWinary';
 import { useLoginData } from '../contexts/LoginDataContext';
 import './Profile.css';
 
 function Login() {
+  const nameInput = useRef();
+  const typeInput = useRef();
+  const appellationInput = useRef();
+  const millesimeInput = useRef();
+
   const { loginData } = useLoginData();
 
   if (loginData == null) {
@@ -21,15 +27,15 @@ function Login() {
         </h1>
         <div className="bottleForm">
           <label className="labelBottle" htmlFor="type">Type</label>
-          <input className="inputBottle" type="text" id="text" name="text" placeholder="Sec" required />
+          <input className="inputBottle" ref={typeInput} type="text" id="text" name="text" placeholder="Sec" required />
           <label className="labelBottle" htmlFor="appellation">Appellation</label>
-          <input className="inputBottle" type="text" id="text" name="text" placeholder="Chinon" required />
+          <input className="inputBottle" ref={appellationInput} type="text" id="text" name="text" placeholder="Chinon" required />
           <label className="labelBottle" htmlFor="year">Millésime</label>
-          <input className="inputBottle" type="text" id="text" name="text" placeholder="2005" required />
+          <input className="inputBottle" ref={millesimeInput} type="text" id="text" name="text" placeholder="2005" required />
           <label className="labelBottle" htmlFor="medal">Récompense/Médaille</label>
           <input className="inputBottle" type="text" id="text" name="text" placeholder="Elu vin de l'année 2010" />
           <label className="labelBottle" htmlFor="quantity">Quantité</label>
-          <input className="inputBottle" type="text" id="text" name="text" placeholder="6" required />
+          <input className="inputBottle" ref={nameInput} type="text" id="text" name="text" placeholder="6" required />
           <label className="labelImage" htmlFor="labelRecto">Etiquette avant</label>
           <input className="inputImage" type="file" id="labelBottle" name="image" placeholder="Ajoutez votre image" />
           <label className="labelImage" htmlFor="labelVerso">Etiquette arrière</label>
@@ -37,7 +43,25 @@ function Login() {
         </div>
         <div className="btnContainer">
           <button className="btnBottle" type="button">Ajouter une autre bouteille</button>
-          <button className="btnBottle" type="submit">Je vends mes bouteilles!</button>
+          <button
+            className="btnBottle"
+            type="submit"
+            onClick={() => {
+              const url = `http://localhost:8000/users/${loginData.userId}/bottles`;
+              axios.post(url, {
+                name: nameInput.current.value,
+                type: typeInput.current.value,
+                appellation: appellationInput.current.value,
+                year: millesimeInput.current.value,
+                reference_id: 1,
+              })
+                .then((response) => {
+                  console.log(response.data);
+                });
+            }}
+          >
+            Je vends mes bouteilles!
+          </button>
         </div>
       </div>
       <ShowWinary />
