5144a1de8ea12681cb2bf608e288949aaac2a213
MarjorieThomassin <79426116+MarjorieThomassin@users.noreply.github.com>
2021-06-23 12:01:22 +0200

    created upload files fields input and axios

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,18 +7,24 @@ import './Profile.css';
 
 function Login() {
   const { loginData } = useLoginData();
-  const [uploadFile, setUploadFile] = useState();
+  const [bottleFrontFile, setBottleFrontFile] = useState();
+  const [bottleBackFile, setBottleBackFile] = useState();
 
-  const changeHandler = (e) => {
-    setUploadFile(e.target.files[0]);
+  const changeFront = (e) => {
+    setBottleFrontFile(e.target.files[0]);
+  };
+
+  const changeBack = (e) => {
+    setBottleBackFile(e.target.files[0]);
   };
 
   const handleSubmit = () => {
     const formData = new FormData();
-    formData.append('File', uploadFile);
+    formData.append('bottleFront', bottleFrontFile);
+    formData.append('bottleBack', bottleBackFile);
     const url = 'http://localhost:8000/upload';
     axios.post(url, formData)
-      .then((response) => (setUploadFile(response.data)));
+      .then((response) => (console.log(response.data)));
   };
 
   if (loginData == null) {
@@ -61,7 +67,9 @@ function Login() {
           <button className="btnBottle" type="button">Ajouter une autre bouteille</button>
           <input className="inputBottle" type="text" id="text" name="text" placeholder="6" required />
           <label className="labelImage" htmlFor="labelRecto">Etiquette avant</label>
-          <input className="inputImage" type="file" id="labelBottle" name="file" placeholder="Ajoutez votre image" onChange={changeHandler} />
+          <input className="inputImage" type="file" id="labelRecto" name="fileFront" placeholder="Ajoutez votre image" onChange={changeFront} />
+          <label className="labelImage" htmlFor="labelVerso">Etiquette arrière</label>
+          <input className="inputImage" type="file" id="labelVerso" name="fileBack" placeholder="Ajoutez votre image" onChange={changeBack} />
         </div>
         <div className="btnContainer">
           <button className="btnBottle" type="button" onClick={handleSubmit}>Ajouter une image</button>
